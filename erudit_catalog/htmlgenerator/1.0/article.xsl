0e2e85b79c3117a9acb5973d92d8d901e053c87a
Fabio Batalha <fabio.batalha@gmail.com>
2018-04-19 16:20:04 -0400

    Fist Commit with Plugin Structure

---
+++ erudit_catalog/htmlgenerator/1.0/article.xsl
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+    xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:mml="http://www.w3.org/1998/Math/MathML"
+    exclude-result-prefixes="xlink mml">
+
+
+    <xsl:output method="html" indent="yes" encoding="UTF-8" omit-xml-declaration="no" xml:space="preserve"/>
+
+    <xsl:template match="/">
+        <html>
+            <body>
+                Érudit Article
+            </body>
+        </html>
+    </xsl:template>
+
+</xsl:stylesheet>
---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.0'
+
+INSTALL_REQUIRES = [
+
+]
+
+setup(
+    name='EruditCatalog',
+    version=VERSION,
+    description="Érudit Schema Catalog for Packtools",
+    author="Érudit",
+    maintainer="Fabio Batalha",
+    maintainer_email="fabio.batalha@erudit.org",
+    license="BSD License",
+    url="http://erudit.org",
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    test_suite='tests',
+    install_requires=INSTALL_REQUIRES,
+    entry_points="""
+    [packtools.catalog]
+    packtools_catalog=erudit_catalog:catalog
+    """,
+)
